60faf5c570023a5699f07f146baf47857ef44dc9
RobertMo <robertmozhaoming@gmail.com>
2023-01-15 20:27:59 -0800

    new donate link

--- src/react/App.js
+++ src/react/App.js
@@ -30,9 +30,13 @@ function App() {
       <Tab>Weight and Balance</Tab>
       <Tab>Q&A</Tab>
       <Tab><form action="https://www.paypal.com/donate" method="post" target="_top">
-      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-      <img alt="" border="0" src="https://www.paypal.com/en_CA/i/scr/pixel.gif" width="1" height="1" />
-      </form></Tab>
+<input type="hidden" name="business" value="VL6X36SRHVQAG" />
+<input type="hidden" name="no_recurring" value="0" />
+<input type="hidden" name="currency_code" value="CAD" />
+<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+<img alt="" border="0" src="https://www.paypal.com/en_CA/i/scr/pixel.gif" width="1" height="1" />
+</form>
+</Tab>
     </TabList>
 
     <TabPanel>
