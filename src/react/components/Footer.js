aebe2af042e3a423db1736ec1611c1779d5e978a
RobertMo <robertmozhaoming@gmail.com>
2023-02-05 21:25:53 -0800

    add logo

--- src/react/components/Footer.js
+++ src/react/components/Footer.js
@@ -1,8 +1,10 @@
-
-
+import logo from '../logo.svg';
+const style={
+    verticalAlign:"middle"
+}
 function Footer(props){
 	return (<div>
-      <h1>Built by Robert the MO</h1>
+      <h1>Built by <img width="50" height="50" src={logo} alt="Logo" style={style} /></h1>
     </div>)
 }
 
--- src/react/components/Header.js
+++ src/react/components/Header.js
@@ -1,6 +1,11 @@
+import logo from '../logo.svg';
+
+const style={
+	verticalAlign:"middle"
+}
 function Header(props){
 	return (<div>
-		<h1>Built by Robert the MO</h1>
+		<h1>Built by <img width="50" height="50" src={logo} alt="Logo" style={style} /></h1> 
     </div>)
 }
 
