e10a17870996a03f9d963c0d7174270dbf6bccc4
RobertMo <robertmozhaoming@gmail.com>
2023-02-05 23:06:18 -0800

    changed website title, centered donate button

--- src/react/App.js
+++ src/react/App.js
@@ -20,6 +20,9 @@ const styles = {
     },
     body:{
       margin: "40px",
+    },
+    donateButton:{
+      verticalAlign:"middle"
     }
 };
 
@@ -37,7 +40,7 @@ function App() {
           <input type="hidden" name="business" value="VL6X36SRHVQAG" />
           <input type="hidden" name="no_recurring" value="0" />
           <input type="hidden" name="currency_code" value="CAD" />
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+          <input style={styles.donateButton} type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
         </form>
       </Tab>
     </TabList>
